fc11431b92563357c7af4ad059ef200e09fc6823
shanuka55 <www.shanukalashan5555@gmail.com>
2024-04-28 12:53:04 +0530

    front end breeds container updated

--- src/component/Breeds.js
+++ src/component/Breeds.js
@@ -1,21 +1,50 @@
 import React from 'react'
 import "../component/Breeds.css";
-import labrodor from '../Assets/labrodor.png';
+
+
 
 const Breeds = () => {
   return (
     <div className='Breeds-container'>
       <div className='container'>
-                <img src={labrodor} alt='john' />
-                <div className='col-2'>
-                    <h2>LABRODOR</h2>
-                    <span className='line'></span>
-                    <p>Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.</p>
-                    <button className='button'>Know more</button>
-                </div>
+
+        {data.map((d) => (
+          <div>
+            <img src={d.img} alt=""/>
+            <div className='col-2'>
+                <h2>{d.name}</h2>
+                <span className='line'></span>
+                <p>{d.review}</p>
+                <button className='button'>Know more</button>
             </div>
+          </div>
+        ))}     
+      </div>
     </div>
   )
 }
 
-export default Breeds
+const data = [
+  {
+    name: 'labrodor',
+    img: `../Assets/labrodor.png`,
+    review: `Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.`
+  },
+  {
+    name: 'Rottweiler',
+    img: `../Assets/Rottweiler.png`,
+    review: `Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.`
+  },
+  {
+    name: 'Beagle',
+    img: `../Assets/Beagle.png`,
+    review: `Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.`
+  },
+  {
+    name: 'Beagle',
+    img: `../Assets/Beagle.png`,
+    review: `Intense is an International Financial Planning company with offices i n multiple jurisdictions over the world. Working with Intense gives me the ability to advise internat ional expatriates living in the middle east from where I live in USA.`
+  }
+]
+
+export default Breeds 
